7533748296a955340f1cde7c8cdac3cda60f8a0b
muqsithck <muqsithck@gmail.com>
2022-09-05 15:18:27 +0530

    responsive ui

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -7,17 +7,22 @@ import Player from '../Player/index'
 const Index = ({ popup, closePopup, channelList, streamType, streamCodec, handleInputChange, loadSetting, inputInfo }) => {
   return (
     <>
-    {
-      popup && <Popup  {...{closePopup, handleInputChange, loadSetting, inputInfo}} />
-    }
-    
-        <div className='main-wrapper' >
-      <div className='video-grid-wrapper'>
-        {
-          channelList ?  channelList.map((item, i) => <Player  {...{item,streamType,streamCodec,inputInfo }}  />) : <>No contetns</>
+      {
+        popup && <Popup  {...{ closePopup, handleInputChange, loadSetting, inputInfo }} />
+      }
+
+      <div className='main-wrapper' >
+        {channelList ?
+          <div className='video-grid-wrapper'>
+            {
+              channelList.map((item, i) => <Player  {...{ item, streamType, streamCodec, inputInfo }} />)
+            }
+          </div>
+          :
+          <div className='message-wrapper' ><p className='message' > Nothing there to stream !!!,
+            Please load setting   </p></div>
         }
       </div>
-    </div>
     </>
   )
 
